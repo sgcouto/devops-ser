2f44c263201d8e34584df5c01a2ef0acdd873298
sgcouto <sgcouto@outlook.com>
2021-10-27 23:30:09 -0300

    Alteração 23:29

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,23 +1,11 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App">      
+      <h1> Devops-ser </h1>
+      <h2> Conteúdo principal</h2>
+      <h3> v1.0.0 </h3>
     </div>
   );
 }
