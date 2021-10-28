b9d356244ca3f5ca7987f7781278723a2b5b3f85
sgcouto <sgcouto@outlook.com>
2021-10-27 23:33:11 -0300

    Alteração Nova tecnologia

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,8 +4,8 @@ function App() {
   return (
     <div className="App">      
       <h1> Devops-ser </h1>
-      <h2> Conteúdo principal</h2>
-      <h3> v1.0.0 </h3>
+      <h2> Prova de conceito de uma nova tecnologia</h2>
+      <h3> v1.1.0 </h3>
     </div>
   );
 }
